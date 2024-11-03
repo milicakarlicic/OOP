31545e8b113abbfff2a9add18575bfc3a6d91a46
milicakarlicic <milicakarlicic1801@gmail.com>
2024-11-03 10:44:08 +0100

    Update

---
+++ cas04/resenja/primer01/Student.java
@@ -0,0 +1,68 @@
+package primer01;
+
+public class Student {
+    private String ime;
+    private String prezime;
+    private String alas;
+    private double prosek;
+
+    public Student(String ime, String prezime, String alas, double prosek) {
+        this.ime = ime;
+        this.prezime = prezime;
+        this.alas = alas;
+        this.prosek = prosek;
+    }
+
+    public String getIme() {
+        return ime;
+    }
+
+    public String getPrezime() {
+        return prezime;
+    }
+
+    public String getAlas() {
+        return alas;
+    }
+
+    public double getProsek() {
+        return prosek;
+    }
+
+    public void setAlas(String alas) {
+        if (validirajAlas(alas)) {
+            this.alas = alas;
+        } else {
+            System.err.println("nevalidan alas");
+        }
+    }
+
+    private boolean validirajAlas(String alas) {
+        if (alas.length() != 7) {
+            System.err.println("nevalidna duzina");
+            return false;
+        } else if (alas.charAt(0) != 'm') {
+            System.err.println("nevalidan modul");
+            return false;
+        } else if (alas.charAt(1) != 'r' && alas.charAt(1) != 'i') {
+            System.err.println("nevalidan smer");
+            return false;
+        } else {
+            for (int i = 2; i < alas.length(); i++) {
+                if (!Character.isDigit(alas.charAt(i))) {
+                    System.err.println("nevalidan indeks");
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+    }
+
+    @Override
+    public String toString() {
+        return ime + " " + prezime + " " + alas + " " + prosek;
+    }
+    
+}
---
+++ cas04/resenja/primer02_03/Elipsa.java
@@ -0,0 +1,35 @@
+package primer02;
+
+public class Elipsa extends Oblik {
+    private double a;
+    private double b;
+
+    public Elipsa(double a, double b) {
+        // super();
+        inic(a, b);
+    }
+
+    public Elipsa(Tacka centar, double a, double b) {
+        super(centar);
+        inic(a, b);
+    }
+
+    public Elipsa(Elipsa e) {
+        super(e.centar);
+        inic(e.a, e.b);
+    }
+
+    public double getA() {
+        return a;
+    }
+
+    private void inic(double a, double b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    @Override
+    public String toString() {
+        return "Elipsa sa poluosama " + a + ", " + b + " i centrom " + centar;
+    }
+}

90ea19b67ae2b45f97cbeabe050458762c92afca
milicakarlicic <milicakarlicic1801@gmail.com>
2024-11-08 18:14:18 +0100

    Update

---
+++ cas05/resenja/primer02/UnarniIzraz.java
@@ -0,0 +1,13 @@
+package primer02;
+
+public abstract class UnarniIzraz extends Izraz {
+    private Izraz operand;
+
+    public UnarniIzraz(Izraz operand) {
+        this.operand = operand.klon();
+    }
+
+    public Izraz getOperand() {
+        return operand;
+    }
+}
